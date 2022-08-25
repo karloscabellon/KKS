284c3d9647e63281bf599c3a78d415c90d7dbd10
karloscabellon <73312815+karloscabellon@users.noreply.github.com>
2022-08-26 00:45:12 +0800

    Add files via upload

--- KKS/settings.py
+++ KKS/settings.py
@@ -125,10 +125,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 MEDIA_URL = "/media/"
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, "static")
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 MEDIA_ROOT = os.path.join(BASE_DIR,"media/")
